de46e3e5f45f0e5f3abc9ccd65d50c844c0092d4
Ross Keeley <ross.keeley@sky.uk>
2022-10-03 17:04:04 +0100

    private/status endpoint with OK 200 status res

---
+++ src/main/java/com/alfonso/CarApp/PrivateStatus.java
@@ -0,0 +1,12 @@
+package com.alfonso.CarApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrivateStatus {
+    @GetMapping("/private/status")
+    public String index() {
+        return "OK";
+    }
+}
