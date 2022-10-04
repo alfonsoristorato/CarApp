ecf260f1fc5f4544bac1323f9ed5fcdf8b2f8c4d
alfonsoristoratoSky <alfonso.ristorato@sky.uk>
2022-10-04 11:53:18 +0100

    added unit tests

--- src/main/java/com/alfonso/CarApp/controllers/PrivateStatus.java
+++ src/main/java/com/alfonso/CarApp/controllers/PrivateStatus.java
@@ -1,4 +1,4 @@
-package com.alfonso.CarApp;
+package com.alfonso.CarApp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/test/java/com/alfonso/CarApp/controllers/PrivateStatusTest.java
@@ -0,0 +1,19 @@
+package com.alfonso.CarApp.controllers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PrivateStatusTest {
+    @InjectMocks
+    PrivateStatus privateStatus;
+
+    @Test
+    void whenPrivateStatusCalled_returnOK() {
+        String response = privateStatus.getPrivateStatus();
+        Assertions.assertEquals("OK", response);
+    }
+}
